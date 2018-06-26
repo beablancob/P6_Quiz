7febe4444605937950ea7c9777d2563369cc6385
beablancob <beablancobejar@gmail.com>
2018-06-26 18:00:40 +0200

    p6

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -153,3 +153,111 @@ exports.check = (req, res, next) => {
         answer
     });
 };
+
+/**
+ * El objetivo de este método es preguntar al cliente los quizzes que todavia no han sido preguntados de forma
+ * aleatoria. Asi, lo primero q comprobamos es que se haya preguntado ya alguna vez al cliente. En caso contrario,
+ * inicializamos el juego guardando variables de score y un almacen de los quizzes.
+ *
+ * Una vez inicializado procedemos a crear un numero aleatorio comprendido entre el numero de quizzes que existe.
+ * Ese numero será el numero del quizz que preguntemos al cliente, saliendo por pantalla éste con su score, en este caso 0.
+ *
+ * Si ya habia jugado anteriormente, cargamos los quizzes que todavía no se han preguntado. Volvemos a preguntar por el
+ * quiz que ha salido aleatoriamente, que sale por pantalla junto con la puntuacion que lleva hasta el momento
+ *
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.randomPlay = function (req, res, next) {
+
+
+
+    console.log("quizzes: " + req.session.quizzes);
+    console.log("score: "+ req.session.score);
+    const {quiz, query} = req;
+    const answer = query.answer || "";
+    var Nalmacen = 0;
+    var almacen = [];
+
+    //creo una variable global score de la sesion:
+
+    //hago lo mismo con un almacen de quizzes
+    if(req.session.quizzes === undefined){
+        req.session.score =0;
+        models.quiz.findAll()
+            .then(quizzes => {
+
+                req.session.quizzes = quizzes;
+
+                console.log("quizzes: " + req.session.quizzes);
+                console.log("score: "+ req.session.score);
+
+                Nalmacen = req.session.quizzes.length;
+                var i = Math.floor(Math.random() * Nalmacen);
+                var quizz = req.session.quizzes[i];
+                req.session.quizzes.splice(i, 1); //elimina el quiz[i] del almacen porque ya va a ser preguntado en la siguiente linea
+                res.render('quizzes/random_play', {
+                    score: req.session.score,
+                    quiz: quizz
+                });
+            })
+            .catch(err => console.log(err));
+
+    }else{
+        almacen = req.session.quizzes;
+        if(almacen.length === 0){
+            var score = req.session.score;
+
+            res.render('quizzes/random_nomore', {score: score});
+        }else{
+            Nalmacen = req.session.quizzes.length;
+            var i = Math.floor(Math.random() * Nalmacen);
+            var quizz = req.session.quizzes[i];
+            req.session.quizzes.splice(i, 1);
+            res.render('quizzes/random_play', {
+                score: req.session.score,
+                quiz: quizz
+            });
+        }
+    }
+};
+
+
+exports.randomCheck = (req, res, next) => {
+
+    const {quiz, query} = req;
+
+    const answer = query.answer || "";
+    const result = answer.toLowerCase().trim() === quiz.answer.toLowerCase().trim();
+    var score;
+
+    if (result) {
+        req.session.score++;
+        score = req.session.score;
+        if(req.session.quizzes.length===0){
+            delete req.session.quizzes;
+            delete req.session.score;
+            res.render('quizzes/random_nomore', {score: score});
+        }else{
+
+            res.render('quizzes/random_result', {
+                answer: answer,
+                quiz: quiz,
+                result: result,
+                score: score
+            });
+        }
+    }else{
+        score = req.session.score;
+        delete req.session.quizzes;
+        delete req.session.score;
+        res.render('quizzes/random_result', {
+            answer: answer,
+            quiz: quiz,
+            result: result,
+            score: score
+        });
+    }
+};
